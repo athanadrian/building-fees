5b554aceac7350edec354bae127979be338ce698
athanadrian <athanadrian@gmail.com>
2018-02-01 19:01:04 +0200

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAqWzYO8jv6kVZrJ_GyjhWBiqf7HKWsGM0",
+    authDomain: "building-fees.firebaseapp.com",
+    databaseURL: "https://building-fees.firebaseio.com",
+    projectId: "building-fees",
+    storageBucket: "building-fees.appspot.com",
+    messagingSenderId: "618723828121"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAqWzYO8jv6kVZrJ_GyjhWBiqf7HKWsGM0",
+    authDomain: "building-fees.firebaseapp.com",
+    databaseURL: "https://building-fees.firebaseio.com",
+    projectId: "building-fees",
+    storageBucket: "building-fees.appspot.com",
+    messagingSenderId: "618723828121"
+  }
 };
